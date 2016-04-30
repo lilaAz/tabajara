1849210b410e3a862c1153bdb8a08515370e1956
lilaAz <lilah.azevedo13@gmail.com>
2016-04-30 18:34:15 -0300

    Iniciando o Projeto

---
+++ js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('inCioCtrl', function($scope) {
+
+})
+   
+.controller('oCursoCtrl', function($scope) {
+
+})
+   
+.controller('aEmpresaCtrl', function($scope) {
+
+})
+      
+.controller('contatoCtrl', function($scope) {
+
+})
+   
+.controller('produtoCtrl', function($scope) {
+
+})
+ 
